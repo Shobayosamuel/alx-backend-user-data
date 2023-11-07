7f3fb616ee82454e98fee0689f88e00703232eb5
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-07 22:24:37 +0000

    task 8: Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,7 @@ Module for authentication using Basic auth
 
 from typing import TypeVar
 from api.v1.auth.auth import Auth
-import base64
+import base64, binascii
 
 from models.user import User
 
@@ -24,3 +24,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Return the decoded value for the header if it  not None"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decode_val = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(decode_val)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
