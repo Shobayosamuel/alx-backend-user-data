f9c996d6fd0e29bb14e5c965eb86a4ccaa5fed14
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-07 23:38:56 +0000

    task 10: Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,3 +52,19 @@ class BasicAuth(Auth):
         else:
             first, sec = decoded_base64_authorization_header.split(":")
             return (first, sec)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Return: user with the valid password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({"email": user_email})
+        if users == [] or users is None:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return None
