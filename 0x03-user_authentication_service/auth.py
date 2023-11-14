2bf5d432feba8dba51ef3f0c8c4592d3e55f19fd
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-14 22:23:07 +0000

    feat: Register new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 """Function to hash password"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialize the new auth"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user if the email does not exist"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+
+        else:
+            raise ValueError('User {} already exists'.format(email))
 
 
 def _hash_password(password: str) -> bytes:
