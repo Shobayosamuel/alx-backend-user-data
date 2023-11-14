3c6b3ca120563a0d5b9b5e6eb4c186e83bfdbff8
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-14 21:52:09 +0000

    fix: adher to annotated style and reduced lines of code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,11 +3,8 @@
 import bcrypt
 
 
-def _hash_password(password) -> bytes:
+def _hash_password(password: str) -> bytes:
     """Hash password using bcrypt
         return: bytes
     """
-    pass_byt = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(pass_byt, salt)
-    return hashed
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
