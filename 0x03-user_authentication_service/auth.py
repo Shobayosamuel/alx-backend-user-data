f14ea86d41af46537d8e057e97007eafc6f4ceef
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-15 19:40:26 +0000

    fix pycodestyle error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,6 +40,7 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def _generate_uuid(self) -> str:
+
+def _generate_uuid() -> str:
     """Return string representation of uuid"""
     return str(uuid.uuid4())
