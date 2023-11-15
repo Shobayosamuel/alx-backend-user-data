86df4aee46f1394339e08db93407fa1c099be5d4
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-15 20:13:04 +0000

    create login and add cookie

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,7 +32,7 @@ class Auth:
         except Exception:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
-    
+ 
     def create_session(self, email: str) -> str:
         """Create a user session id and return it"""
         try:
