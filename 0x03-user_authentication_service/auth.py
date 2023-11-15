8563c6881eb8d0b09162fbd203bdd16d681f14ee
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-15 20:48:06 +0000

    feat: destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,6 +53,16 @@ class Auth:
             return None
         else:
             return user
+    
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except Exception:
+            return None
+        else:
+            user.session_id = None
+            return None
 
 
 def _hash_password(password: str) -> bytes:
