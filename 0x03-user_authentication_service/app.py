f16bc5769573f4716031c6f55c6f73a5efe39828
Shobayo Samuel <Shobayosamuel62@gmail.com>
2023-11-15 22:41:45 +0000

    fix documentation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,6 +80,7 @@ def get_reset_password_token() -> str:
 
 @app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
+    """End-point to update password"""
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
